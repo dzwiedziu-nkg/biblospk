ed0e4b1a42a82df5abcca4fa8dfd57248da9b081
nkg <nkg753@gmail.com>
2016-03-16 16:25:46 +0100

    Fix register in AlarmManager.

--- app/src/main/java/pl/nkg/biblospk/MyApplication.java
+++ app/src/main/java/pl/nkg/biblospk/MyApplication.java
@@ -2,11 +2,20 @@ package pl.nkg.biblospk;
 
 import com.activeandroid.app.Application;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
+
 import pl.nkg.biblospk.client.WebClient;
+import pl.nkg.biblospk.receivers.AlarmReceiver;
 import pl.nkg.biblospk.services.BiblosService;
 import pl.nkg.biblospk.services.NotifyService;
 
 public class MyApplication extends Application {
+
+    private static final int PERIOD = 3 * 60 * 60 * 1000;
 
     private GlobalState mGlobalState;
 
@@ -18,9 +27,20 @@ public class MyApplication extends Application {
 
         BiblosService.startService(this, false, false);
         NotifyService.startService(this);
+        registerAlarm();
     }
 
     public GlobalState getGlobalState() {
         return mGlobalState;
+    }
+
+    private void registerAlarm() {
+        AlarmManager mgr = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        PendingIntent pi = PendingIntent.getBroadcast(this, 0, new Intent(this, AlarmReceiver.class), 0);
+
+        mgr.setRepeating(AlarmManager.ELAPSED_REALTIME,
+                SystemClock.elapsedRealtime() + 60000,
+                PERIOD,
+                pi);
     }
 }
