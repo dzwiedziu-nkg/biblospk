dcca95ed90f7f20c8efbf3140c06291cef09c22c
nkg <nkg753@gmail.com>
2016-03-26 03:08:58 +0100

    Force refresh book list when alarm received and WiFi connected

--- app/src/main/java/pl/nkg/biblospk/receivers/AlarmReceiver.java
+++ app/src/main/java/pl/nkg/biblospk/receivers/AlarmReceiver.java
@@ -4,11 +4,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import pl.nkg.biblospk.Utils;
 import pl.nkg.biblospk.services.BiblosService;
 
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        BiblosService.startService(context, false, true);
+        BiblosService.startService(context, Utils.checkWiFi(context), true);
     }
 }
