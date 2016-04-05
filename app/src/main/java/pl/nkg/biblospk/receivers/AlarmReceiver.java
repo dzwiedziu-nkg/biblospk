12a227ad19061f0dacba8a0da6410606852a3ba1
nkg <nkg753@gmail.com>
2016-04-05 08:26:37 +0200

    AlarmReceiver download only one per day

--- app/src/main/java/pl/nkg/biblospk/receivers/AlarmReceiver.java
+++ app/src/main/java/pl/nkg/biblospk/receivers/AlarmReceiver.java
@@ -4,12 +4,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import pl.nkg.biblospk.Utils;
 import pl.nkg.biblospk.services.BiblosService;
 
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        BiblosService.startServiceRefresh(context, Utils.checkWiFi(context), true);
+        BiblosService.startServiceRefresh(context, false, true);
     }
 }
