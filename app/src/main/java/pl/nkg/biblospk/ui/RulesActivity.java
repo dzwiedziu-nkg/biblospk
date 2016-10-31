d2037f24bcbc18125332242b80b7204168c30c8f
nkg <nkg753@gmail.com>
2016-10-31 09:21:46 +0100

    English translation of rules.

--- app/src/main/java/pl/nkg/biblospk/ui/RulesActivity.java
+++ app/src/main/java/pl/nkg/biblospk/ui/RulesActivity.java
@@ -1,5 +1,7 @@
 package pl.nkg.biblospk.ui;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -16,12 +18,29 @@ public class RulesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rules);
         TextView textView = (TextView) findViewById(R.id.textView);
 
+        PackageInfo pInfo;
+        try {
+            pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
         Spanned rules;
+        StringBuilder sb = new StringBuilder();
+        sb.append(getText(R.string.rules));
+        sb.append("<p>");
+        sb.append(getText(R.string.app_name));
+        sb.append(" v");
+        sb.append(pInfo.versionName).append(" (build ").append(pInfo.versionCode).append(")<br/>");
+        sb.append(getText(R.string.copyright));
+        sb.append("<br/>");
+        sb.append(getText(R.string.url));
+        sb.append("</p>");
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-            rules = Html.fromHtml(getText(R.string.rules).toString(), Html.FROM_HTML_MODE_LEGACY);
+            rules = Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY);
         } else {
-            rules = Html.fromHtml(getText(R.string.rules).toString());
+            rules = Html.fromHtml(sb.toString());
         }
 
         textView.setText(rules);
