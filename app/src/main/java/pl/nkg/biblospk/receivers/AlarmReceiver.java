897caa9c9175016f5beaebe3939dfa48dfa34111
nkg <nkg753@gmail.com>
2016-11-04 17:29:44 +0100

    Notification update to alarm receiver.

--- app/src/main/java/pl/nkg/biblospk/receivers/AlarmReceiver.java
+++ app/src/main/java/pl/nkg/biblospk/receivers/AlarmReceiver.java
@@ -1,14 +1,18 @@
 package pl.nkg.biblospk.receivers;
 
+import org.greenrobot.eventbus.EventBus;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import pl.nkg.biblospk.events.UpdateNotifyEvent;
 import pl.nkg.biblospk.services.BiblosService;
 
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         BiblosService.startServiceRefresh(context, false, true);
+        EventBus.getDefault().post(new UpdateNotifyEvent());
     }
 }
